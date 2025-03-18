9db60a2da991627d235d24d1173edb55a2408ef0
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-03-18 21:37:47 +0300

    premier pas avec express

--- NODE-POKEMON-API/app.js
+++ NODE-POKEMON-API/app.js
@@ -1,1 +1,8 @@
-console.log("Hello world 👍");
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello, Express ! 👍'))
+
+app.listen(port, () => console.log(`Notre Application node est démarée sur : http://localhost:${port} `))
