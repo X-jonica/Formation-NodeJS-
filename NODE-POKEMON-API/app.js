e267a5149ece3495cc2f40041894b5279fbb072d
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-03-21 17:52:54 +0300

    pratique avec le retour JSON et exercice d'application relié a cela

--- NODE-POKEMON-API/app.js
+++ NODE-POKEMON-API/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
+const { success } = require("./helper.js");
 let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello , Express ! 😒");
-});
+app.get("/", (req, res) => res.send("Hello , Express ! 😒"));
 
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
-
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  res.json(pokemon);
+  const message = "Un pokemon a bien été trouver";
+  res.json(success(message, pokemon));
 });
+
+app.get('/api/pokemons', (req, res) => {
+  const message = "Nous avons bien recuperé les 12 pokemons dans le pokedex."
+  res.json(success(message, pokemons))
+})
 
 app.listen(port, () => {
   console.log(`Notre application est demaréé sur : http://localhost:${port}`);
