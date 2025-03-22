bbaec263593a67185c35da2aa4ab639eb2d99256
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-03-22 15:20:33 +0300

    premiere push

--- NODE-POKEMON-API/app.js
+++ NODE-POKEMON-API/app.js
@@ -9,15 +9,17 @@ app.get("/", (req, res) => res.send("Hello , Express ! 😒"));
 
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
+
+  
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
   const message = "Un pokemon a bien été trouver";
   res.json(success(message, pokemon));
 });
 
-app.get('/api/pokemons', (req, res) => {
-  const message = "Nous avons bien recuperé les 12 pokemons dans le pokedex."
-  res.json(success(message, pokemons))
-})
+app.get("/api/pokemons", (req, res) => {
+  const message = "Nous avons bien recuperé les 12 pokemons dans le pokedex.";
+  res.json(success(message, pokemons));
+});
 
 app.listen(port, () => {
   console.log(`Notre application est demaréé sur : http://localhost:${port}`);
