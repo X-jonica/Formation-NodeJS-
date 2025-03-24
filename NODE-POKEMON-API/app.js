201e3361ca5359abb605513a2a5a6ffa8f62fbee
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-03-24 10:15:49 +0300

    refaire l'exercice du dernier commit

--- NODE-POKEMON-API/app.js
+++ NODE-POKEMON-API/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morgan = require("morgan")
+const morgan = require("morgan");
 const favicon = require("serve-favicon");
 let pokemons = require("./mock-pokemon");
 const { success, getUniqueId } = require("./helper");
@@ -7,15 +7,13 @@ const { success, getUniqueId } = require("./helper");
 const app = express();
 const port = 3000;
 
-// Middleware existant 
-app
-  .use(favicon(__dirname+ '/favicon.ico'))
-  .use(morgan("dev"));
+// Middleware existant
+app.use(favicon(__dirname + "/favicon.ico")).use(morgan("dev"));
 
-// hello Express 
+// hello Express
 app.get("/", (req, res) => res.send("Hello , Express ! 😒"));
 
-// get les pokemon par id 
+// get les pokemon par id
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
@@ -23,19 +21,20 @@ app.get("/api/pokemons/:id", (req, res) => {
   res.json(success(message, pokemon));
 });
 
-// get tous les pokemons 
-app.get('/api/pokemons', (req, res) => {
-  const message = "La liste des pokemons a été bien recueré !"
-  res.json(success(message, pokemons))
-})
+// get tous les pokemons
+app.get("/api/pokemons", (req, res) => {
+  const message = "La liste des pokemons a été bien recueré !";
+  res.json(success(message, pokemons));
+});
 
-app.post('/api/pokemons', (req, res) => {
-  const id = 123 ;
-  const pokemonCreated = {...req.body, ...{id: id, created: new Date()}};
-  pokemons.push(pokemonCreated);
-  const message = `le pokemon ${pokemonCreated.name} a ete bien ajouté :)`;
-  res.json(success(message, pokemonCreated))
-})
+// premere methode post 
+  app.post('/api/pokemons', (req, res) => {
+    const id = 123 ;
+    const pokemonCreated = {...req.body, ...{id: id , created: new Date()}};
+    pokemons.push(pokemonCreated);
+    const message = `le pokemon ${pokemonCreated.name} a été bien créé !`;
+    res.json(success(message, pokemonCreated))
+  })
 
 app.listen(port, () => {
   console.log(`Notre application est demaréé sur : http://localhost:${port}`);
